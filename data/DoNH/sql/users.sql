8d9e3d0062e1734d1c596c5ccdab630404d5f9a0
hoanghd <mikoashi@gmail.com>
2016-05-04 22:49:04 +0700

    update

---
+++ data/DoNH/sql/users.sql
@@ -0,0 +1,16 @@
+CREATE TABLE `users` (
+  `user_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `password` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `email` varchar(100) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `birthday` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `avatar` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `is_active` smallint(6) NOT NULL DEFAULT '1',
+  `ip` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `lastvisit` int(11) unsigned NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT NULL,
+  `modified_at` timestamp NULL DEFAULT NULL,
+  `token` text,
+  `token_created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
