718e6d9d3c748a9c64bce190f37405c91cdc77dd
hoanghd <hoanghd>
2016-05-09 08:51:52 +0700

    update

--- data/DoNH/php/asset/js/main.js
+++ data/DoNH/php/asset/js/main.js
@@ -5,77 +5,101 @@ if('undefined' == typeof(VS1K5716)){
 $this = VS1K5716;
 
 VS1K5716.signUp = function(){
-	var form = $("#signUpForm");
-	if($this.valid(form)){
-		form.get(0).submit();	
-	}
+    var form = $("#signUpForm");
+    if($this.valid(form)){
+        form.get(0).submit();    
+    }
 };
 
 VS1K5716.login = function(){
-	var form = $("#loginForm");
-	if($this.valid(form)){
-		form.get(0).submit();	
-	}
+    var form = $("#loginForm");
+    if($this.valid(form)){
+        form.get(0).submit();    
+    }
 };
 
 VS1K5716.lostPassword = function(){
-	var form = $("#lostForm");
-	if($this.valid(form)){
-		form.get(0).submit();	
-	}
+    var form = $("#lostForm");
+    if($this.valid(form)){
+        form.get(0).submit();    
+    }
 };
 
 VS1K5716.changePassword = function(){
-	var form = $("#changepwForm");
-	if($this.valid(form)){
-		form.get(0).submit();	
-	}
+    var form = $("#changepwForm");
+    if($this.valid(form)){
+        form.get(0).submit();    
+    }
+};
+
+VS1K5716.cloudinary = function(img){
+    var reader = new FileReader();    
+    reader.onloadend = function(){
+       var fd = new FormData();
+       fd.append('upload_preset', 'keothomv2_preset');
+       fd.append('file', reader.result);
+       
+       var xhr = new XMLHttpRequest();
+       xhr.open('POST', 'https://api.cloudinary.com/v1_1/keothom/image/upload', true);
+       
+       xhr.onload = function() {
+           if(this.status == 200){
+             var resp = JSON.parse(this.response);
+             console.dir(resp);
+           };
+       };
+       xhr.send(fd);
+    };
+    
+    if(img){
+      reader.readAsDataURL(img);
+    }
 };
 
 VS1K5716.valid = function(id){
-	var valid = true;
-	
-	$("[valid]", id).each(function(){
-		var elm   = $(this);
-		var val   = $.trim(elm.val());
-		var data  = elm.attr('valid').split('|');
-		
-		$.each(data, function(index, value){
-			if(valid){
-				if(value == 'req' && !val){
-					valid = false;
-					elm.notify("Bạn không được để trống trường này.");
-					return;
-				}
-				else if(value == 'email' && !$this.validation.isEmail(val)){
-					valid = false;
-					elm.notify("Địa chỉ email không hơp lệ.");
-					return;
-				}
-				else if(value == 'username' && !$this.validation.username(val)){
-					valid = false;
-					elm.notify("Vui lòng nhập chữ cái, số và dấu chấm.");
-					return;
-				}
-				else if(value.startsWith('equal') && !$this.validation.equal(val, value, id)){
-					valid = false;
-					elm.notify("Mật khẩu này không khớp.");
-					return;
-				}
-				else if(value.startsWith('length') && !$this.validation.length(val, value, elm)){
-					valid = false;
-					return;
-				}
-				else if(value == 'captcha' && (grecaptcha.getResponse().length == 0)){
-					valid = false;
-					elm.notify("Vui lòng chọn Captcha");
-					return;	
-				}
-			}
-		});
-	});
-	
-	return valid;
+    var valid = true;
+    
+    $("[valid]", id).each(function(){
+        var elm   = $(this);
+        var val   = $.trim(elm.val());
+        var data  = elm.attr('valid').split('|');
+        
+        $.each(data, function(index, value){
+            if(valid){
+                if(value == 'req' && !val){
+                    valid = false;
+                    elm.notify("Bạn không được để trống trường này.");
+                    return;
+                }
+                else if(value == 'email' && !$this.validation.isEmail(val)){
+                    valid = false;
+                    elm.notify("Địa chỉ email không hơp lệ.");
+                    return;
+                }
+                else if(value == 'username' && !$this.validation.username(val)){
+                    valid = false;
+                    elm.notify("Vui lòng nhập chữ cái, số và dấu chấm.");
+                    return;
+                }
+                else if(value.startsWith('equal') && !$this.validation.equal(val, value, id)){
+                    valid = false;
+                    elm.notify("Mật khẩu này không khớp.");
+                    return;
+                }
+                else if(value.startsWith('length') && !$this.validation.length(val, value, elm)){
+                    valid = false;
+                    return;
+                }
+                else if(value == 'captcha' && (grecaptcha.getResponse().length == 0)){
+                    valid = false;
+                    elm.notify("Vui lòng chọn Captcha");
+                    return;    
+                }
+            }
+        });
+    });
+    
+    return valid;
 };
 
 VS1K5716.replace = function(str, data){
@@ -85,42 +109,42 @@ VS1K5716.replace = function(str, data){
 };
 
 VS1K5716.validation = {
-	isEmail: function(email){
-		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(email);
-	},
-	username: function(val){
-		var re = /^[a-zA-Z0-9\.]+$/;
-		return re.test(val);
-	}	,
-	equal: function(val, value, id){
-		var compareVal = $(value.substring(5), id).val();
-		return (compareVal == val);
-	},
-	length: function(val, value, elm){
-		var cond = JSON.parse(value.substring(6).replace(/\'/gi, '"'));
-		var len = val.length;
-		if(cond.min && cond.max){
-			if(len<cond.min || len>cond.max){
-				elm.notify($this.replace("Vui lòng sử dụng từ {min} đến {max} ký tự.", cond));
-				return false;
-			}
-		}
-		else if(cond.min){
-			if(len<cond.min){
-				elm.notify($this.replace("Vui lòng sử dụng ít nhất {min} ký tự.", cond));
-				return false;
-			}
-		}
-		else if(cond.max){
-			if(len>cond.max){
-				elm.notify($this.replace("Vui lòng sử dụng nhiều nhất {max} ký tự.", cond));
-				return false;
-			}
-		}
-		
-		return true;
-	}
+    isEmail: function(email){
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    },
+    username: function(val){
+        var re = /^[a-zA-Z0-9\.]+$/;
+        return re.test(val);
+    }    ,
+    equal: function(val, value, id){
+        var compareVal = $(value.substring(5), id).val();
+        return (compareVal == val);
+    },
+    length: function(val, value, elm){
+        var cond = JSON.parse(value.substring(6).replace(/\'/gi, '"'));
+        var len = val.length;
+        if(cond.min && cond.max){
+            if(len<cond.min || len>cond.max){
+                elm.notify($this.replace("Vui lòng sử dụng từ {min} đến {max} ký tự.", cond));
+                return false;
+            }
+        }
+        else if(cond.min){
+            if(len<cond.min){
+                elm.notify($this.replace("Vui lòng sử dụng ít nhất {min} ký tự.", cond));
+                return false;
+            }
+        }
+        else if(cond.max){
+            if(len>cond.max){
+                elm.notify($this.replace("Vui lòng sử dụng nhiều nhất {max} ký tự.", cond));
+                return false;
+            }
+        }
+        
+        return true;
+    }
 };
 
 if(!String.prototype.startsWith){
@@ -132,14 +156,14 @@ if(!String.prototype.startsWith){
 
 $(document).ready(function(){
    $(".vs1k5716 .nav a").click(function(){
-		$('.vs1k5716 .nav a').removeClass('active');  	
-		$(this).addClass('active'); 
-		
-		$('.vs1k5716 .tab-content > .tab-pane').addClass('hide');
-		$($(this).attr('ref')).removeClass('hide');
+        $('.vs1k5716 .nav a').removeClass('active');      
+        $(this).addClass('active'); 
+        
+        $('.vs1k5716 .tab-content > .tab-pane').addClass('hide');
+        $($(this).attr('ref')).removeClass('hide');
    });
    
    if($("input.datepicker").length>0){
-		$("input.datepicker").datepick({dateFormat: 'yyyy-mm-dd'});   
+        $("input.datepicker").datepick({dateFormat: 'yyyy-mm-dd'});   
    }
 });
