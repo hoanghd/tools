a3b2695fa286efec76bed2ad50978e0a62e98303
hoanghd <mikoashi@gmail.com>
2016-07-25 17:59:52 +0700

    update

--- wordpress/plugin/validation/js/main.js
+++ wordpress/plugin/validation/js/main.js
@@ -43,7 +43,7 @@ var NewAddress = Backbone.View.extend({
 	initialize: function() {
 		this.render();
 		this.render1();
-		this.listenTo( this.model, 'change:name', this.render1 );
+		this.listenTo( this.model, 'change:name.*', this.render1 );
 		Backbone.Validation.bind(this);
 	},
 	render: function() {
