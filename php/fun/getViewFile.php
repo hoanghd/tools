24b86b51875be3391113db22de3ce34651631923
Huynh Duc Hoang <mikoashi@gmail.com>
2016-10-27 08:57:16 +0700

    Update getViewFile.php

--- php/fun/getViewFile.php
+++ php/fun/getViewFile.php
@@ -5,23 +5,12 @@
  * @param string $view_name view name
  * @return string the view file path, false if the view file does not exist
  */
-public function get_view_file( $view_name ) {
+public function getViewFile( $viewName ) {
     if( is_file( $view_name ) && file_exists( $view_name ) ) {
         return $view_name;
     }
 
-    $folder = 'views';
-    if( preg_match( '/^(?P<folder>\w+)\.(?P<view_name>\w+)$/', $view_name, $mt ) ) {
-        $folder  .= '/' . $mt['folder'];
-        $view_name = $mt['view_name'];
-    }
-
     $class = new ReflectionClass( get_called_class() );
-
-    if( ($folder == 'views') && preg_match( '/(?P<ctr>[^\-]+)\.php$/', basename( $class->getFileName() ), $mt ) ){
-        $folder .= '/' . $mt['ctr'];
-    }
-
-    return realpath( dirname( $class->getFileName() ) . "/{$folder}/{$view_name}.php" );
+    return realpath( dirname( $class->getFileName() ) . "/{$viewName}.php" );
 }
 ?>
