9ac882c92c271431dbe9ed1dbdd53e79c69a68e8
Huynh Duc Hoang <mikoashi@gmail.com>
2016-10-27 08:20:32 +0700

    Update render.php

--- php/fun/render.php
+++ php/fun/render.php
@@ -1,5 +1,5 @@
 <?php
-  /**
+        /**
 	 * Renders a view file.
 	 * This method includes the view file as a PHP script
 	 * and captures the display result if required.
@@ -8,21 +8,21 @@
 	 * @param boolean $_return_ whether the rendering result should be returned as a string
 	 * @return string the rendering result. Null if the rendering result is not required.
 	 */
-	public function renderInternal($_viewFile_,$_data_=null,$_return_=false)
-	{
+	function render( $_viewFile_, $_data_ = null, $_return_ = false ) {
 		// we use special variable names here to avoid conflict when extracting data
-		if(is_array($_data_))
-			extract($_data_,EXTR_PREFIX_SAME,'data');
-		else
-			$data=$_data_;
-		if($_return_)
-		{
-			ob_start();
-			ob_implicit_flush(false);
-			require($_viewFile_);
-			return ob_get_clean();
+		if( is_array( $_data_ ) ) {
+			extract( $_data_, EXTR_PREFIX_SAME, 'data' );
+		} else {
+			$data = $_data_;
 		}
-		else
-			require($_viewFile_);
+		
+		if( $_return_ ) {
+			ob_start();
+			ob_implicit_flush( false );
+			require( $_viewFile_ );
+			return ob_get_clean();
+		} else {
+			require( $_viewFile_ );
+		}
 	}
 ?>
