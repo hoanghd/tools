2e72c5ae0b220744fcdbf13ead6bd750d376ad29
Huynh Duc Hoang <mikoashi@gmail.com>
2017-06-19 13:08:01 +0700

    Update UserMailer.php

--- CakePHP & Laravel/CakePHP.v3/Sample Code/UserMailer.php
+++ CakePHP & Laravel/CakePHP.v3/Sample Code/UserMailer.php
@@ -14,10 +14,7 @@ class EntriesMailer extends Mailer{
             ->emailFormat('text');
     }
 }
-
-
-
----------
+---------------
 use Cake\Mailer\MailerAwareTrait;
         
 class DefaultController extends AppController
@@ -30,3 +27,30 @@ class DefaultController extends AppController
                 ->send('created', [1]);
     }
 }
+---------------
+app.php
+---------------
+'Email' => [
+    'default' => [
+        'transport' => 'gmail',
+        'from' => 'no-return@coating.kurumaerabi.com',
+        'charset' => 'ISO-2022-JP',
+        //'headerCharset' => 'utf-8',
+    ],
+],
+
+'gmail'=> [
+    'host' => 'ssl://smtp.gmail.com',
+    'port' => 465,
+    'username' => 'mikoashi@gmail.com',
+    'password' => '----',
+    'className' => 'Smtp',
+    'log' => true,
+    'context' => [
+      'ssl' => [
+          'verify_peer' => false,
+          'verify_peer_name' => false,
+          'allow_self_signed' => true
+      ]
+    ]
+],
